88c5cb981a9139b7d856a6c2a409a7fdbc5a2307
HritikaJ <hritikajoshi188@gmail.com>
2020-12-17 18:46:28 +0545

    First Commit

---
+++ Author.aspx.cs
@@ -0,0 +1,141 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LMS3
+{
+    public partial class Author : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //default load data
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+            }
+        }
+        private void BindGrid()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            OracleCommand cmd = new OracleCommand();
+            OracleConnection con = new OracleConnection(constr);
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT ID, Name, Gender, Qualification FROM Author";
+            cmd.CommandType = CommandType.Text;
+            DataTable dt = new DataTable("author");
+
+            using (OracleDataReader sdr = cmd.ExecuteReader())
+            {
+                dt.Load(sdr);
+            }
+
+            con.Close();
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+
+        protected void bthInsert_Click(object sender, EventArgs e)
+        {
+            string name = txtAuthor.Text.ToString();
+            string gender = txtGender.Text.ToString();
+            string qualification = txtQualification.Text.ToString();
+
+            string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            using (OracleConnection con = new OracleConnection(constr))
+            {
+                using (OracleCommand cmd = new OracleCommand("Insert into author(Name, Gender, Qualification)Values('" + name + "','" + gender + "','" + qualification + "')"))
+                {
+
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    txtAuthor.Text = "";
+                    txtGender.Text = "";
+                    txtQualification.Text = "";
+
+                }
+
+            }
+
+
+            this.BindGrid();
+        }
+
+        protected void OnRowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            int ID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            string Name = (row.Cells[2].Controls[0] as TextBox).Text;
+            string Gender = (row.Cells[3].Controls[0] as TextBox).Text;
+            string Qualification = (row.Cells[4].Controls[0] as TextBox).Text;
+            string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            using (OracleConnection con = new OracleConnection(constr))
+            {
+                using (OracleCommand cmd = new OracleCommand("update author set Name = '" + Name + "',Gender = '" + Gender + "',Qualification = '" + Qualification + "' where ID = " + ID))
+                {
+
+                    cmd.Connection = con;
+                    con.Open();
+
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+
+                }
+            }
+
+            GridView1.EditIndex = -1;
+            this.BindGrid();
+
+        }
+        protected void OnRowCancelingEdit(object sender, EventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            this.BindGrid();
+        }
+        protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int ID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            string constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            using (OracleConnection con = new OracleConnection(constr))
+            {
+                using (OracleCommand cmd = new OracleCommand("DELETE FROM Author WHERE ID =" + ID))
+                {
+
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            this.BindGrid();
+
+        }
+        protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow && e.Row.RowIndex != GridView1.EditIndex)
+            {
+                (e.Row.Cells[0].Controls[2] as LinkButton).Attributes["onclick"] = "return confirm('Do you want to delete this row?');";
+            }
+
+        }
+        protected void OnRowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            this.BindGrid();
+        }
+    }
+}
